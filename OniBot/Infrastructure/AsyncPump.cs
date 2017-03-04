c05277853de23d627f8f30c19dda0cd169626810
Chris Curwick <ccurwick@exoronet.net>
2017-03-04 09:33:55 -0800

    Set up console application to support async/await pattern

---
+++ OniBot/Infrastructure/AsyncPump.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OniBot.Infrastructure
+{
+    public class AsyncPump
+    {
+        public delegate Task SingleArgumentDelegate(object args);
+        public static void Run(SingleArgumentDelegate func, object args)
+        {
+            if (func == null) throw new ArgumentNullException(nameof(func));
+
+            var prevCtx = SynchronizationContext.Current;
+
+            try
+            {
+                var syncCtx = new SingleThreadSynchronizationContext();
+                var task = func(args).ContinueWith(a =>
+                {
+                    syncCtx.Complete();
+                }, TaskScheduler.Default);
+
+                syncCtx.RunOnCurrentThread();
+
+                if(task.IsFaulted) {
+                    var edi = ExceptionDispatchInfo.Capture(task.Exception);
+                    edi.Throw();
+                }
+
+                task.GetAwaiter().GetResult();
+            }
+            finally
+            {
+                SynchronizationContext.SetSynchronizationContext(prevCtx);
+            }
+        }
+
+        private class SingleThreadSynchronizationContext : SynchronizationContext
+        {
+            private readonly BlockingCollection<KeyValuePair<SendOrPostCallback, object>> _queue = new BlockingCollection<KeyValuePair<SendOrPostCallback, object>>();
+
+            public override void Post(SendOrPostCallback callback, object state)
+            {
+                if (callback == null) throw new ArgumentNullException(nameof(callback));
+                _queue.Add(new KeyValuePair<SendOrPostCallback, object>(callback, state));
+            }
+
+            public override void Send(SendOrPostCallback d, object state) => throw new NotSupportedException();
+
+            public void RunOnCurrentThread()
+            {
+                foreach (var workItem in _queue.GetConsumingEnumerable())
+                {
+                    workItem.Key(workItem.Value);
+                }
+            }
+
+            public void Complete() => _queue.CompleteAdding();
+        }
+    }
+}
---
+++ OniBot/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+using OniBot.Infrastructure;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OniBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ctx = new SynchronizationContext();
+
+            AsyncPump.Run(a => MainAsync(args), args);
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            try
+            {
+                var commandLine = new ConfigurationBuilder();
+                commandLine.AddCommandLine(args);
+                var commandLineConfig = commandLine.Build();
+
+                var environment = commandLineConfig["environment"]?.ToLower() ?? "production";
+                var config = new ConfigurationBuilder();
+                config.AddJsonFile("config.json", false);
+                config.AddJsonFile($"config.{environment}.json", true);
+                config.AddEnvironmentVariables();
+                if (environment == "development")
+                {
+                    config.AddUserSecrets();
+                }
+                config.AddInMemoryCollection(commandLineConfig.AsEnumerable());
+
+                var configuration = config.Build();
+
+                foreach (var key in configuration.AsEnumerable())
+                {
+                    Console.WriteLine($"{key.Key.PadRight(40)}: {key.Value}");
+                }
+
+                await Task.Yield();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
