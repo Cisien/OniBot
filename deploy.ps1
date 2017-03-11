baca05168bf3f3f3bdf5d474bafe0d4f48a92829
Chris Curwick <ccurwick@exoronet.net>
2017-03-10 17:19:55 -0800

    appveyor

--- deploy.ps1
+++ deploy.ps1
@@ -4,10 +4,16 @@ $ErrorActionPreference = 'Stop';
 #  Write-Host "No version tag detected. Skip publishing."
 #  exit 0
 #}
+$tag = $env:APPVEYOR_REPO_TAG_NAME
 
+if([System.String]::IsNullOrWhitespace($tag)) {
+    $tag = "untagged"
+}
+
+$env | out-host
 Write-Host Starting deploy
 docker login -u="$env:DOCKER_USER" -p="$env:DOCKER_PASS"
 
-docker tag onibot cisien/onibot:$env:APPVEYOR_REPO_TAG_NAME
+docker tag onibot cisien/onibot:$tag
 
-docker push cisien/onibot:$env:APPVEYOR_REPO_TAG_NAME
+docker push cisien/onibot:$tag
